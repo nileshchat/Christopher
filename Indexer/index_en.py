c488c6f1fff2fa196dd0bcc8f6cba35f919915d0
nilesh4145 <n.chaturvedi3@gmail.com>
2017-04-10 15:11:53 +0530

    Indexer implementation done!

--- Indexer/index_en.py
+++ Indexer/index_en.py
@@ -1,3 +1,4 @@
+from inverted_index import Inverted_index
 import os
 import re
 from porter2stemmer import Porter2Stemmer
@@ -5,18 +6,14 @@ import string
 
 base_dir = os.getcwd()
 foldername = "en_test"
-stemmed_l = []
+postingListDict = Inverted_index()
 
 for document in os.listdir(os.path.join(base_dir, foldername)):
 
-    # Here .split() does the tokenization task
     doc = re.sub('<[^<]+>',"", open(base_dir+"/"+ foldername +"/{}".format(document), 'r', buffering = 1).read())
-    ts = str.maketrans('','', string.punctuation)
-    punctuated = (doc.translate(ts)).split()
-    #perform operation
-    stemmer = Porter2Stemmer()
-    for word in punctuated:
-        stemmed = stemmer.stem(word.lower())
-        stemmed_l.append(stemmed)
+    punctuated = postingListDict.remove_punctuations(doc)
+    stemmed = postingListDict.stem(punctuated)
+    chopped_stopwords = postingListDict.remove_stop_words(stemmed)
+    index = postingListDict.create_index(chopped_stopwords, document)
 
-    print(stemmed_l)
+    print(index)
--- Indexer/inverted_index.py
+++ Indexer/inverted_index.py
@@ -1,34 +1,71 @@
-import porter2stemmer as ps
+from porter2stemmer import Porter2Stemmer
 import string
 
+#Create a list of stop words
+
 stop_word_list = []
-
 wordlist = open('stopwords_en.txt', 'r').readlines()
-
 for word in wordlist:
     stop_word = word[:(len(word)-1)]
     stop_word_list.append(str(stop_word))
 
+
 class Inverted_index():
+
     def __init__(self):
         self.postingList = {}
         self.document = []
 
     def stem(self, list_of_words):
-        stemmer = ps()
-        stemmed = stemmer.stem(list_of_words)
+    # returns a list of stemmed words
 
-        return stemmed
+        stemmed_list = []
+        stemmer = Porter2Stemmer()
+
+        for word in list_of_words:
+            stemmed_word = stemmer.stem(word.lower())
+            stemmed_list.append(stemmed_word)
+
+        return stemmed_list
+
     def remove_punctuations(self, sentence_string):
-        #returns a list of words with punctuations removed in them
+    #returns a list of words with punctuations removed in them
 
-        translator = str.maketrans('', '', string.punctuations)
+        translator = str.maketrans('', '', string.punctuation)
+
         return (sentence_string.translate(translator)).split()
+
     def remove_stop_words(self, tokenized_document):
-        # returns a list with all the stop words removed
+    # returns a list with all the stop words removed
 
         without_stop_words = []
+
         for word in tokenized_document:
             if word not in stop_word_list:
                 without_stop_words.append(word)
+
+        return without_stop_words
+
+    def create_index(self, document, document_name):
+    #returns index
+        unique_words = list(set(document))
+
+        for newWord in unique_words:
+            term_frequency = document.count(newWord)
+            if (len(self.postingList) != 0):
+                if newWord in self.postingList.keys():
+                    doc_freq_tup = tuple([document_name, term_frequency])
+                    self.postingList[newWord].append(doc_freq_tup)
+                else:
+                    doc_freq_tup = tuple([document_name, term_frequency])
+                    self.postingList[newWord] = []
+                    self.postingList[newWord].append(doc_freq_tup)
+            else:
+                doc_freq_tup = tuple([document_name, term_frequency])
+                self.postingList[newWord] = []
+                self.postingList[newWord].append(doc_freq_tup)
+
+        return self.postingList
+
+
         
